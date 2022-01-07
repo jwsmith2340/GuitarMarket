f76e483d5636b115ef059d9f0e554eca93934266
James Smith <jwsmith2340@yahoo.com>
2022-01-07 15:41:20 -0500

    expanded seed, made divs to separate products on display page, working on spacing now

--- models/productSeed.js
+++ models/productSeed.js
@@ -13,4 +13,39 @@ module.exports = [
         frets: 22,
         image: 'https://www.mylespaul.com/attachments/1562324506935-min-jpeg.390890/',
     },
+    {
+        brand: 'Fender',
+        model: 'American Pro Stratocaster', 
+        color: '3-Color Sunburst',
+        frets: 22,
+        image: 'https://c1.zzounds.com/media/productmedia/fit,2018by3200/quality,85/1_Full_Straight_Front_NA-af9df8291d3e1dee80e2f3f98a016fe8.jpg',
+    },
+    {
+        brand: 'Gibson',
+        model: 'Slash Les Paul Standard', 
+        color: 'Anaconda Burst',
+        frets: 22,
+        image: 'https://c1.zzounds.com/media/productmedia/fit,2018by3200/quality,85/1_Full_Straight_Front_45716-8147d77f194f67547da9e6fe6d618dce.jpg'
+    },
+    {
+        brand: 'Gibson',
+        model: 'Slash Les Paul Standard', 
+        color: 'November Burst',
+        frets: 22,
+        image: 'https://c1.zzounds.com/media/productmedia/fit,2018by3200/quality,85/1_Full_Straight_Front_45221-fa0ac9a51dbe0d998e6a29618ddb0811.jpg'
+    },
+    {
+        brand: 'Fender',
+        model: 'Player Plus Telecaster', 
+        color: 'Cosmic Jade',
+        frets: 22,
+        image: 'https://c1.zzounds.com/media/productmedia/fit,2018by3200/quality,85/0147332376_fen_ins_frt_1_rr_839028-b8157a092eaf617a6e57c57b429e0169.jpg'
+    },
+    {
+        brand: 'EVH',
+        model: 'Striped Series', 
+        color: 'Black and Yellow',
+        frets: 22,
+        image: 'https://c1.zzounds.com/media/productmedia/fit,2018by3200/quality,85/1_Full_Straight_Front_NA-fec262a690085c244e978643faca3ac1.jpg'
+    }
 ]
