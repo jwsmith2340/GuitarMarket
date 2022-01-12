4d2239a176c2be1a2247129b0d1dc2d462d3d0ce
James Smith <jwsmith2340@yahoo.com>
2022-01-11 21:59:38 -0500

    heroku issues

--- server.js
+++ server.js
@@ -41,18 +41,6 @@ app.use(function(req, res, next) {
 })
 //^^custom middleware that lets us log the session to the console
 
-/*
-    vvv Found while trying to fix my Heroku Deployment Issues vvv
-*/
-// Use the session middleware
-app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }}))
-
-// Access the session as req.session
-
-/*
-    ^^^ Found while trying to fix my Heroku Deployment Issues ^^^
-*/
-
 //Controller route
 app.use('/product_category', productsController)
 app.use('/users', usersController)
