67a9d435b4e701ead54e9492bdeb8a4043418cb2
James Smith <jwsmith2340@yahoo.com>
2022-01-13 10:32:24 -0500

    added timestamps to user model

--- models/user.js
+++ models/user.js
@@ -27,7 +27,8 @@ const userSchema = new Schema(
         email: {type: String, unique: true, required: true},
         password: {type: String, required: true},
         cart: [cartSchema]
-    }
+    }, 
+    { timestamps: true }
 )
 
 module.exports = mongoose.model('User', userSchema)
